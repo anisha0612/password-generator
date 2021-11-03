25b3a5b18d6f581e826519797dad08db901fcf56
Anisha <anisha.0612@live.com>
2021-11-03 14:43:06 -0400

    adding generate password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length: number = 0;
+  password: string = "";
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
+  // Get password length from user input
+
+  getLength(event: any){
+    const parsedValue = parseInt(event.target.value);
+
+    if(!isNaN(parsedValue)){
+      this.length= parsedValue;
+      
+    }
+  }
+
+  // Change events for all checkboxes (Letters, Numbers, Symbols)
+  onChangeNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeLetters(){
+    this.includeLetters=!this.includeLetters;
+  }
+
+  onChangeSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+
+  onButtonClick(){
+    // console.log(` Length: ${this.length}, include letters: ${this.includeLetters}, include numbers: ${this.includeNumbers}, include symbols: ${this.includeSymbols}`)
+
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols= "!@#$%^&*()";
+
+    let validChars = "";
+    
+    if(this.includeLetters){
+      validChars+= letters;
+    }
+
+    if(this.includeNumbers){
+      validChars+= numbers;
+    }
+
+    if(this.includeSymbols){
+      validChars+= symbols;
+    }
+
+
+    let generatedPassword= "";
+    for(let i=0; i < this.length; i++){
+      const index : number = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
